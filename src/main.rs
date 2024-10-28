9f4ae69472022f797bdaed105d7e1d07b0e417f6
John Harrington <jharringtonmn@icloud.com>
2024-10-27 20:12:43 -0600

    pdfgen working with imags

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
+use chrono::NaiveDate;
 use genpdf::{elements, style, Alignment, Element};
 use indicatif::{ProgressBar, ProgressStyle};
 use serde::Deserialize;
-use std::fs::File;
-use chrono::NaiveDate;
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{self, Read},
+};
 
 #[derive(Debug, Deserialize)]
 struct Post {
     title: String,
     content: String,
     date: String,
-    // images: Vec<String>,
+    images: Vec<String>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let image_data: HashMap<String, Vec<u8>> = load_image_data("images.bin")?;
     let mut posts: Vec<Post> = serde_json::from_reader(File::open("backup.json")?)?;
-    posts.sort_by_key(|post| {
-        NaiveDate::parse_from_str(&post.date, "%A %d %B %Y").ok()
-    });
-    
+    posts.sort_by_key(|post| NaiveDate::parse_from_str(&post.date, "%A %d %B %Y").ok());
+
     let font_family =
         genpdf::fonts::from_files("./fonts", "OpenSans", None).expect("Failed to load font family");
 
@@ -46,40 +49,73 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .styled(title_style);
         doc.push(title_element);
 
-        let date_style = style::Style::new().with_font_size(12);
+        let date_style = style::Style::new().with_font_size(12).bold();
         let date_element = elements::Paragraph::new(post.date.clone())
             .aligned(Alignment::Left)
             .styled(date_style);
         doc.push(date_element);
 
         doc.push(elements::Break::new(1));
-        let content_lines = post.content.lines(); 
+        let content_lines = post.content.lines();
         for line in content_lines {
             let content_element = elements::Paragraph::new(line.replace("&amp;", "&").to_string());
             doc.push(content_element);
-        }        
+        }
 
-        // Add images (commented out as per your request)
-        // for image_url in &post.images {
-        //     bar.set_message(format!("Downloading image for: {}", post.title));
-        //     if let Ok(image_data) = download_image(image_url) {
-        //         let image = genpdf::elements::Image::from_dynamic_image(image::load_from_memory(&image_data)?)?;
-        //         page.push(image);
-        //     }
-        // }
+        for image_url in &post.images {
+            match image_data.get(image_url) {
+                Some(image_data) => {
+                    let image_data = image::load_from_memory(&image_data)?;
+                    if image_data.color().has_alpha() {
+                        continue; // images with alpha channel not supported
+                    }
+                    let image = genpdf::elements::Image::from_dynamic_image(image_data)?
+                        .with_alignment(genpdf::Alignment::Center);
+
+                    doc.push(image);
+                }
+                None => {
+                    bar.set_message(format!("Downloading image for: {}", post.title));
+                    match download_image(image_url) {
+                        Ok(image_data) => {
+                            let loaded_image = image::load_from_memory(&image_data)?;
+                            if loaded_image.color().has_alpha() {
+                                continue; // images with alpha channel not supported
+                            }
+                            let image = genpdf::elements::Image::from_dynamic_image(loaded_image)?
+                                .with_alignment(genpdf::Alignment::Center);
+                            doc.push(image);
+                        }
+                        Err(e) => {
+                            eprintln!("Failed to download image: {}", e);
+                        }
+                    }
+                }
+            }
+        }
 
         bar.inc(1);
     }
 
     bar.finish_with_message("PDF generation complete!");
-
-    let output_file = File::create("Blog.pdf")?;
+    println!("Saving PDF, this may take a few minutes...");
+    let output_file = File::create("Blog1.pdf")?;
     doc.render(&mut std::io::BufWriter::new(output_file))?;
+    println!("All done!");
 
     Ok(())
 }
 
-// fn download_image(url: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-//     let response = reqwest::blocking::get(url)?;
-//     Ok(response.bytes()?.to_vec())
-// }
+fn load_image_data(path: &str) -> io::Result<HashMap<String, Vec<u8>>> {
+    let mut file = File::open(path)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    let decoded: HashMap<String, Vec<u8>> =
+        bincode::deserialize(&buffer).expect("Deserialization failed");
+    Ok(decoded)
+}
+
+fn download_image(url: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let response = reqwest::blocking::get(url)?;
+    Ok(response.bytes()?.to_vec())
+}
